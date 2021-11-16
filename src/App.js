a7ce76af132c4352c6d778e45df3d6a4206ecc22
daniel teo <teodaniel14@gmail.com>
2021-11-16 12:35:52 +0800

    added react elements for Pet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Potato"),
+    React.createElement("h3", {}, "Dog"),
+    React.createElement("h3", {}, "Shiba Inu")
+  ])
+}
+
 const App = () => {
   return React.createElement(
     "div",
     {},
-    React.createElement("h1", {}, "Adopt Me!")
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet),
+      React.createElement(Pet),
+      React.createElement(Pet)
+    ]
   );
 };
 
